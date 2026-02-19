c19d8204b174a92ea74ef53c75c8c34d8a194b31
amine <daharmedamine@outlook.com>
2026-02-19 16:20:30 -0500

    lifter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/lifter_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/lifter_test.java
@@ -10,6 +10,13 @@ public class lifter_test extends LinearOpMode {
 
     private CRServo myServo;
     private CRServo myServo1;
+    private CRServo myServo2;
+    private CRServo myServo3;
+    private CRServo myServo4;
+    private CRServo myServo5;
+    private CRServo myServo6;
+    private CRServo myServo7;
+
 
     @Override
     public void runOpMode() {
@@ -20,6 +27,13 @@ public class lifter_test extends LinearOpMode {
         try {
             myServo = hardwareMap.get(CRServo.class, "myServo");
             myServo1 = hardwareMap.get(CRServo.class, "myServo1");
+            myServo2 = hardwareMap.get(CRServo.class, "myServo2");
+            myServo3 = hardwareMap.get(CRServo.class, "myServo3");
+            myServo4 = hardwareMap.get(CRServo.class, "myServo4");
+            myServo5 = hardwareMap.get(CRServo.class, "myServo5");
+            myServo6 = hardwareMap.get(CRServo.class, "myServo6");
+            myServo7 = hardwareMap.get(CRServo.class, "myServo7");
+
 
             // If the code reaches here, it found them!
             telemetry.addData("Servos Detected", "YES");
@@ -42,17 +56,38 @@ public class lifter_test extends LinearOpMode {
                 if (gamepad1.a) {
                     // Move UP
                     myServo.setPower(1);
-                    myServo1.setPower(-1);
+                    myServo1.setPower(1);
+                    myServo2.setPower(1);
+                    myServo3.setPower(1);
+                    myServo4.setPower(1);
+                    myServo5.setPower(1);
+                    myServo6.setPower(1);
+                    myServo7.setPower(1);
+
                 }
                 else if (gamepad1.b) {
                     // Move DOWN
                     myServo.setPower(-1);
-                    myServo1.setPower(1); // Fixed: changed myServo to myServo1
+                    myServo1.setPower(-1);
+                    myServo2.setPower(-1);
+                    myServo3.setPower(-1);
+                    myServo4.setPower(-1);
+                    myServo5.setPower(-1);
+                    myServo6.setPower(-1);
+                    myServo7.setPower(-1);
+
                 }
                 else {
                     // STOP
                     myServo.setPower(0);
-                    myServo1.setPower(0); // Fixed: changed myServo to myServo1
+                    myServo1.setPower(0);
+                    myServo2.setPower(0);
+                    myServo3.setPower(0);
+                    myServo4.setPower(0);
+                    myServo5.setPower(0);
+                    myServo6.setPower(0);
+                    myServo7.setPower(0);
+                    
                 }
 
                 // Keep showing the status while running
